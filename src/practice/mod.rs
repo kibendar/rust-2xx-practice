4bdc183448cd7ea5ee443f9441ae8e0bde160726
kidenbar <danyakononenko14@gmail.com>
2024-10-25 09:50:49 +0300

    do task lower_uppers_cases

--- src/practice/mod.rs
+++ src/practice/mod.rs
@@ -2,3 +2,4 @@ mod rhombus;
 mod envelope;
 mod gcd;
 mod tree;
+mod lower_upper_cases;
---
+++ target/debug/deps/rust_2xx_practice.d
@@ -0,0 +1,12 @@
+C:\rust-2xx-practice\target\debug\deps\rust_2xx_practice.exe: src/main.rs src\rust_by_practice\mod.rs src\rust_by_practice\chapter3.rs src\rust_by_practice\chapter4.rs src\rust_by_practice\chapter5.rs src\rust_by_practice\chapter6.rs src\practice\mod.rs src\practice\simple_array_sum.rs
+
+C:\rust-2xx-practice\target\debug\deps\rust_2xx_practice.d: src/main.rs src\rust_by_practice\mod.rs src\rust_by_practice\chapter3.rs src\rust_by_practice\chapter4.rs src\rust_by_practice\chapter5.rs src\rust_by_practice\chapter6.rs src\practice\mod.rs src\practice\simple_array_sum.rs
+
+src/main.rs:
+src\rust_by_practice\mod.rs:
+src\rust_by_practice\chapter3.rs:
+src\rust_by_practice\chapter4.rs:
+src\rust_by_practice\chapter5.rs:
+src\rust_by_practice\chapter6.rs:
+src\practice\mod.rs:
+src\practice\simple_array_sum.rs:
